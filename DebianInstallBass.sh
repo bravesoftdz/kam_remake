61a114858456a42ac87af563de43251fc7b35427
Thimo Braker <thibmorozier@live.nl>
2018-04-10 15:53:06 +0200

    Add more dependencies and def requirements to the script

--- DebianInstallBass.sh
+++ DebianInstallBass.sh
@@ -4,4 +4,6 @@ chmod a+rx /usr/local/lib/libbass.so
 ldconfig
 
 apt-get update
-apt-get install libasound2 libopenal1 libalut0 libgles2-mesa
+apt-get install libasound2 libopenal1 libalut0 libglu1-mesa libgles1-mesa libgles2-mesa libgl2ps1 libglew2.0 libglfw3
+# For dev:
+# apt-get install libasound2-dev libopenal-dev libalut-dev libgl1-mesa-dev libglu1-mesa-dev libgles1-mesa-dev libgles2-mesa-dev libgl2ps-dev libglew-dev libglfw3-dev
